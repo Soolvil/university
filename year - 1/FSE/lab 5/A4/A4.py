15abcd0a73f5ba89be4bdf126e5e743c7f2af6f4
Pupkevich Dmitry <Pupkevich_DS_25@student.grsu.by>
2025-11-25 12:21:33 +0300

    Переписал функции и вывод в A4 5-ой практической

--- year - 1/FSE/lab 5/A4/A4.py
+++ year - 1/FSE/lab 5/A4/A4.py
@@ -3,6 +3,10 @@ import sys
 counter = 1
 separator = "--------------------------------------------------------------------------"
 
+input_data = (open(sys.argv[1], "r")).readlines()
+commands = (open(sys.argv[2], "r")).readlines()
+output_file = open(sys.argv[3], "w")
+
 def encode(text):
     res = ""
     buff = ""
@@ -19,87 +23,93 @@ def encode(text):
 
 def decode(seq):
     text = ""
-    for i in range(0, len(seq)):
+    i = 0
+    while i < len(seq):
         if(seq[i] >= '3' and seq[i] <= '9'):
             text += seq[i + 1] * int(seq[i])
-            i += 1
+            i += 2
             continue
         text += seq[i]
+        i += 1
     return text
 
-def search(data, seq):
-    seq = decode(seq)
-    print("organism				protein")
-    found = False
+def search_seq(data, key):
     for protein in data:
-        name, organism, amino = protein.split('\t')
-        if amino.find(seq) != -1:
-            found = True
-            print(f"{organism}          {name}")
-    if not found:
-        print("NOT FOUND")
+        _, _, amino = protein
+        if amino.find(key) != -1:
+            return protein
+    return ("", "", "")
 
-def diff(data, name1, name2):
-    amino1 = ""
-    amino2 = ""
+def search_name(data, key):
     for protein in data:
-        name, _, amino = protein.split('\t')
-        amino = amino[:-1]
-        if name == name1:
-            amino1 = decode(amino)
-        if name == name2:
-            amino2 = decode(amino)
-    if amino1 == "" or amino2 == "":
-        print("MISSING:")
-        if amino1 == "":
-            print(name1)
-        if amino2 == "":
-            print(name2)
-    else:
-        res = abs(len(amino1) - len(amino2))
-        for i in range(0, min(len(amino1), len(amino2))):
-            if amino1[i] != amino2[i]:
-                res += 1
-        print("amino-acids difference:")
-        print(res)
+        name, _, _ = protein
+        if name == key:
+            return protein
+    return ("", "", "")
 
-def mode(data, mname):
-    seq = ""
-    for protein in data:
-        name, _, amino = protein.split('\t')
-        if name == mname:
-            seq = decode(amino)
-    if seq == "":
-        print("MISSING:")
-        print(mname)
-    else:
-        acids = {f"{seq[0]}":0}
-        for i in range(0, len(seq)):
-            if seq[i] not in acids:
-                acids[seq[i]] = 0
-            acids[seq[i]] += 1
-        maximal = seq[0]
-        for name, count in acids.items():
-            if count > acids[maximal] or (count == acids[maximal] and name < maximal):
-                maximal = name
-        print("amino-acid occurs:")
-        print(f"{maximal}          {acids[maximal]}")
+def diff(amino1, amino2):
+    res = abs(len(amino1) - len(amino2))
+    for i in range(0, min(len(amino1), len(amino2))):
+        if amino1[i] != amino2[i]:
+            res += 1
+    return res
 
-data = (open(sys.argv[1], "r")).readlines()
-commands = (open(sys.argv[2], "r")).readlines()
-print("Pupkevich Dzmitry\nGenetic Searching")
+def mode(seq):
+    acids = {f"{seq[0]}":0}
+    for i in range(0, len(seq)):
+        if seq[i] not in acids:
+            acids[seq[i]] = 0
+        acids[seq[i]] += 1
+    maximal = seq[0]
+    for name, count in acids.items():
+        if count > acids[maximal] or (count == acids[maximal] and name < maximal):
+            maximal = name
+    return (maximal, acids[maximal])
+
+print("Pupkevich Dzmitry\nGenetic Searching", file = output_file)
+data = []
+for i in range(0, len(input_data)):
+    temp = input_data[i].split('\t')
+    temp[-1] = temp[-1][:-1]
+    data.append((temp[0], temp[1], temp[2]))
+#print(data)
+#print("\n\n\n\n")
 for line in commands:
-    print(separator)
+    print(separator, file = output_file)
     arguments = line.split('\t')
     arguments[-1] = arguments[-1][:-1]
     if arguments[0] == "search":
-        print(f"{counter:0>3}   search   {arguments[1]}")
-        search(data, arguments[1])
+        print(f"{counter:0>3}   search   {decode(arguments[1])}", file = output_file)
+        print("organism                 protein", file = output_file)
+        res = search_seq(data, decode(arguments[1]))
+        name, organism, _ = res
+        if name != "":
+            print(f"{organism}     {name}", file = output_file)
+        else:
+            print("NOT FOUND", file = output_file)
     elif arguments[0] == "diff":
-        print(f"{counter:0>3}   diff   {arguments[1]}   {arguments[2]}")
-        diff(data, arguments[1], arguments[2])
+        print(f"{counter:0>3}   diff   {arguments[1]}   {arguments[2]}", file = output_file)
+        first = search_name(data, arguments[1])[2]
+        second = search_name(data, arguments[2])[2]
+        if first != "" and second != "":
+            res = diff(first, second)
+            print("amino-acids difference:", file = output_file)
+            print(f"{res}", file = output_file)
+        else:
+            print("MISSING:", file = output_file)
+            if first == "":
+                print(arguments[1])
+            if second == "":
+                print(arguments[2])
     elif arguments[0] == "mode":
-        print(f"{counter:0>3}   mode   {arguments[1]}")
-        mode(data, arguments[1])
+        print(f"{counter:0>3}   mode   {arguments[1]}", file = output_file)
+        acid_seq = search_name(data, arguments[1])[2]
+        if acid_seq != "":
+            amino, count = mode(acid_seq)
+            print("amino-acid occurs:", file = output_file)
+            print(f"{amino}          {count}", file = output_file)
+        else:
+            print("MISSING:", file = output_file)
+            print({arguments[1]}, file = output_file)
     counter += 1
-print(separator)
+print(separator, file = output_file)
